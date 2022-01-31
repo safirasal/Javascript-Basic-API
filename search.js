333d135ac8ce6e603b58c1d91dfabdcdd1962b5c
safirasal <safira_salsabila_29rpl@student.smktelkom-mlg.sch.id>
2022-01-31 11:08:35 +0700

    update

---
+++ search.js
@@ -0,0 +1,44 @@
+const { request, response } = require("express")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+// nomer 1
+// endpoint 1
+app.post("/search", (request, response) => {
+    let keyword = request.body.keyword
+    let data = [
+        {nis: 101, nama:"Baskara", alamat:"Surabaya"},
+        {nis: 102, nama:"Aksara", alamat:"Jakarta"},
+        {nis: 103, nama:"Haikal", alamat:"Jogja"},
+        {nis: 104, nama:"Naya", alamat:"Bali"},
+        {nis: 105, nama:"Aruna", alamat:"Bandung"},
+        {nis: 106, nama:"Cici", alamat:"Surabaya"},
+        {nis: 107, nama:"Bayu", alamat:"Malang"},
+        {nis: 108, nama:"Nasa", alamat:"Bekasi"},
+        {nis: 109, nama:"Alvino", alamat:"Tangerang"},
+        {nis: 110, nama:"Dara", alamat:"Bogor"}
+    ]
+
+    let found = []
+    for (let i = 0; i < data.length; i++) {
+        if (keyword == data[i].nis || keyword == data[i].nama || keyword == data[i].alamat) {
+            
+            found.push({
+                nis : data[i].nis,
+                nama : data[i].nama,
+                alamat : data[i].alamat
+            })
+        }
+        
+    }
+
+    return response.json({
+        found
+    })
+})
+
+app.listen(8000, () => {
+    console.log(`Server run on port 8000`);
+})
